7bae8c2f1786fd8cfd5a84e736f31bb86b57de17
Mohamed Metwally <112720711+MohamedMetwally01@users.noreply.github.com>
2023-11-29 04:21:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import streamlit as st
-import os
-import time
-import glob
-from gtts import gTTS
-from googletrans import Translator
 
-# Create 'temp' directory if it doesn't exist
+
 
 st.write("Hello, my name is Mohamed Metwally")
 st.write("and the first project")
